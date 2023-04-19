e648479c167d5b4f0baaee61bc466a6809f8cbce
Colby <me@cobeh.com>
2023-04-19 15:31:23 -0700

    fixing issues with clang format

--- src/programs/hello_world/main.cpp
+++ src/programs/hello_world/main.cpp
@@ -1,24 +1,15 @@
 // Copyright Colby Hall. All Rights Reserved.
 
 #include <iostream>
-#include <vector>
 
-#define SHORT 1
+#define SHORT			1
 #define SUPER_LONG_NAME 12
 
 auto main(int argc, char** argv) -> int {
-    (void)argc;
-    (void)argv;
+	(void)argc;
+	(void)argv;
 
-    std::cout << "Hello World" << 4 << std::endl;
+	std::cout << "Hello World" << 4 << std::endl;
 
-    void* foo = nullptr;
-
-    return 0;
+	return 0;
 }
-
-struct foo {};
-
-struct Foo_Bar {
-    int x;
-};
