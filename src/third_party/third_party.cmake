0b613c9af90e8e7697bd35513d3f31a1aa566a07
Colby <me@cobeh.com>
2024-04-21 20:33:12 -0700

    Removed imgui and editor library

--- src/third_party/third_party.cmake
+++ src/third_party/third_party.cmake
@@ -1,4 +1,3 @@
 include(${THIRD_PARTY_ROOT}/doctest/doctest.cmake)
 include(${THIRD_PARTY_ROOT}/dxc/dxc.cmake)
-include(${THIRD_PARTY_ROOT}/imgui/imgui.cmake)
 include(${THIRD_PARTY_ROOT}/stb/stb.cmake)
